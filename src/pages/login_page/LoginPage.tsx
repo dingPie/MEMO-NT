36f320326751a05e1f2f3854ab1581400ec61187
pie <cpie1216@gamil.com>
2022-06-05 21:56:03 +0900

    login page css set

--- src/pages/login_page/LoginPage.tsx
+++ src/pages/login_page/LoginPage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+// Components
 import Header from "../../components/Header";
 import { MobileBox } from "../../components/MobileBox";
+import { ColBox } from "../../components/FlexBox";
+import { CustomBtn } from "../../components/Buttons";
+import Text from "../../components/Text";
 
+// Page Component
+import WithoutLogin from "./WithoutLogin";
+import WithSnsLogin from "./WithSnsLogin";
 
 const LoginPage = () => {
  
@@ -11,11 +20,25 @@ const LoginPage = () => {
       page="login"
     />
     <MobileBox>
-      로그인페이지입니다.
+      <ColBox gap={3.5} padding="4rem 1.5rem">
+        <Text center bold size="3x" >
+          LOGIN
+        </Text>
+      
+        <WithoutLogin />
+ 
+        <WithSnsLogin />
 
+      </ColBox>
     </MobileBox>
     </>
   )
 }
 
 export default LoginPage;
+
+export const LoginBtn = styled(CustomBtn)`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 2rem 1fr ;
+`
