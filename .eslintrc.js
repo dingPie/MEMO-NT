3f1d4f59de7e1e825afe323decb059d3ffb44a1e
dingPie <cpie1216@gmail.com>
2022-08-28 20:32:24 +0900

    eslint 적용 오류 해결중, windows에서 테스트 후 main 적용 예정.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
-// const process = require("process");
-
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-      },
-    "root": true,
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
-        "@typescript-eslint",
-        "Prettier"
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "Prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+
+  rules: {
+    "linebreak-style": [
+      "error",
+      process.platform === "win32" ? "windows" : "unix",
     ],
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended"
-    ],
-    "rules": {
-     // windows linebreaks when not in production environment
-     "linebreak-style": ["error", process.platform === "win32" ? "unix" : "windows"]    }
-}
+  },
+};
