72d7573f842367df2c87d112704bd1a1b4da6f88
dingPie <cpie1216@gmail.com>
2022-09-02 12:07:16 +0900

    eslint 설정관련 여러 문제 해결. 그동안eslint / prettier가 제대로 적용되고있지 않았음

--- src/App.tsx
+++ src/App.tsx
@@ -1,126 +1,127 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
-import GlobalStyle from "./styles/GlobalStyle";
-import useStore from "./store/useStore";
+import React, { Suspense, useEffect, useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyle';
+import useStore from './store/useStore';
 
 // Page
-import LoginPage from "./pages/login_page/LoginPage";
-import SettingPage from "./pages/setting_page/SettingPage";
-import TalkPage from "./pages/talk_page/TalkPage";
-import GridPage from "./pages/grid_page/GridPage";
-import MemoPage from "./pages/memo_page/MemoPage";
+import LoginPage from './pages/login_page/LoginPage';
+import SettingPage from './pages/setting_page/SettingPage';
+import TalkPage from './pages/talk_page/TalkPage';
+import GridPage from './pages/grid_page/GridPage';
+import MemoPage from './pages/memo_page/MemoPage';
 
 // firebase
-import { FbMemo } from "./firebase/firestore_memo_service";
-import { FbAuth } from "./firebase/firebase_auth_service";
-import { User } from "firebase/auth";
-import { FbTag } from "./firebase/firestore_tag_service";
+import { FbMemo } from './firebase/firestore_memo_service';
+import { FbAuth } from './firebase/firebase_auth_service';
+import { User } from 'firebase/auth';
+import { FbTag } from './firebase/firestore_tag_service';
 
-import { ITag, IUserInfo } from "./utils/interface/interface";
-import { MobileBox } from "./components/MobileBox";
-import IndexPage from "./pages/index_page/IndexPage";
+import { ITag, IUserInfo } from './utils/interface/interface';
+import { MobileBox } from './components/MobileBox';
+import IndexPage from './pages/index_page/IndexPage';
 
 export interface IApp {
-  fbAuth: FbAuth;
-  fbTag: FbTag;
-  fbMemo: FbMemo;
+	fbAuth: FbAuth;
+	fbTag: FbTag;
+	fbMemo: FbMemo;
 }
 
 const App = ({ fbAuth, fbTag, fbMemo }: IApp) => {
-  const { palette, loading } = useStore();
-  const navigate = useNavigate();
-  const [user, setUser] = useState<User | null | false>(false);
-  const [userInfo, setUserInfo] = useState<IUserInfo | null>(null);
-  const [tags, setTags] = useState<ITag[]>([]);
+	const { palette, loading } = useStore();
+	const navigate = useNavigate();
+	const [user, setUser] = useState<User | null | false>(false);
+	const [userInfo, setUserInfo] = useState<IUserInfo | null>(null);
+	const [tags, setTags] = useState<ITag[]>([]);
 
-  // 유저 가입여부 체크 및 가입
-  const CheckAndInitUser = async (user: User) => {
-    const joinedResult = await fbAuth.getUserInfo(user);
-    if (joinedResult) return;
-    // loading.start();
-    console.log("새로 가입을 진행합니다");
-    await fbAuth.addUser(user);
-    await fbTag.initTag(); // user.uid
+	// 유저 가입여부 체크 및 가입
+	const CheckAndInitUser = async (user: User) => {
+		const joinedResult = await fbAuth.getUserInfo(user);
+		if (joinedResult) return;
+		// loading.start();
+		console.log('새로 가입을 진행합니다');
+		await fbAuth.addUser(user);
+		await fbTag.initTag(); // user.uid
 
-    const initMemoId = await fbMemo.initMemo(); // user.uid
-    await fbTag.addUsedMemo("undefined", initMemoId!.undefinedMemoId);
-    await fbTag.addUsedMemo("toBeDeleted", initMemoId!.toBeDeletedMemoId);
-    await fbTag.addUsedMemoAll("initMenual", initMemoId!.initMenualMemoId);
-    // loading.finish();
-    window.location.reload(); // 첫 유저의 경우, user 정보를 받아오는 것 보다 메모 init이 느린 관계로, 새로고침 작업
-  };
+		const initMemoId = await fbMemo.initMemo(); // user.uid
+		await fbTag.addUsedMemo('undefined', initMemoId!.undefinedMemoId);
+		await fbTag.addUsedMemo('toBeDeleted', initMemoId!.toBeDeletedMemoId);
 
-  // 메모 로그인시 설정 초기화
-  const initAppLogin = async (user: User) => {
-    const paletteObj = await fbAuth.getPalette(); // 팔레트 설정
-    palette.setPalette(paletteObj);
-    fbAuth.setUid(user); // uid 의존성 주입
-    fbTag.setDoc(user); // uid 의존성 주입
-    fbMemo.setDoc(user); // uid 의존성 주입
+		await fbTag.addUsedMemoAll('initMenual', initMemoId!.initMenualMemoId);
+		// loading.finish();
+		window.location.reload(); // 첫 유저의 경우, user 정보를 받아오는 것 보다 메모 init이 느린 관계로, 새로고침 작업
+	};
 
-    fbTag.onCheckTag(setTags); // 태그정보 실시간체크
-    fbAuth.onCheckUserInfo(setUserInfo); // UserDB 정보 실시간체크
-    navigate("/talk", { replace: true });
-  };
+	// 메모 로그인시 설정 초기화
+	const initAppLogin = async (user: User) => {
+		const paletteObj = await fbAuth.getPalette(); // 팔레트 설정
+		palette.setPalette(paletteObj);
+		fbAuth.setUid(user); // uid 의존성 주입
+		fbTag.setDoc(user); // uid 의존성 주입
+		fbMemo.setDoc(user); // uid 의존성 주입
 
-  useEffect(() => {
-    fbAuth.onCheckUser(setUser);
-    if (user) {
-      CheckAndInitUser(user);
-      initAppLogin(user);
-    }
-    if (user === null) navigate("/login");
-  }, [user]);
+		fbTag.onCheckTag(setTags); // 태그정보 실시간체크
+		fbAuth.onCheckUserInfo(setUserInfo); // UserDB 정보 실시간체크
+		navigate('/talk', { replace: true });
+	};
 
-  useEffect(() => {
-    console.log("로딩상태 출력", loading.isLoading);
-  }, [loading.isLoading]);
+	useEffect(() => {
+		fbAuth.onCheckUser(setUser);
+		if (user) {
+			CheckAndInitUser(user);
+			initAppLogin(user);
+		}
+		if (user === null) navigate('/login');
+	}, [user]);
 
-  return (
-    <MobileBox>
-      <GlobalStyle />
+	useEffect(() => {
+		console.log('로딩상태 출력', loading.isLoading);
+	}, [loading.isLoading]);
 
-      <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route
-          path="/login"
-          element={<LoginPage fbAuth={fbAuth} setUser={setUser} />}
-        />
-        <Route
-          path="/setting"
-          element={<SettingPage user={user as User} fbAuth={fbAuth} />}
-        />
-        <Route
-          path="/talk"
-          element={
-            <TalkPage
-              fbAuth={fbAuth}
-              fbTag={fbTag}
-              fbMemo={fbMemo}
-              tags={tags}
-              userInfo={userInfo}
-            />
-          }
-        />
-        <Route
-          path="/grid"
-          element={<GridPage fbMemo={fbMemo} fbTag={fbTag} tags={tags} />}
-        />
-        <Route
-          path="/memo/:tagId"
-          element={
-            <MemoPage
-              fbAuth={fbAuth}
-              fbMemo={fbMemo}
-              fbTag={fbTag}
-              tags={tags}
-              userInfo={userInfo}
-            />
-          }
-        />
-      </Routes>
-    </MobileBox>
-  );
+	return (
+		<MobileBox>
+			<GlobalStyle />
+
+			<Routes>
+				<Route path="/" element={<IndexPage />} />
+				<Route
+					path="/login"
+					element={<LoginPage fbAuth={fbAuth} setUser={setUser} />}
+				/>
+				<Route
+					path="/setting"
+					element={<SettingPage user={user as User} fbAuth={fbAuth} />}
+				/>
+				<Route
+					path="/talk"
+					element={
+						<TalkPage
+							fbAuth={fbAuth}
+							fbTag={fbTag}
+							fbMemo={fbMemo}
+							tags={tags}
+							userInfo={userInfo}
+						/>
+					}
+				/>
+				<Route
+					path="/grid"
+					element={<GridPage fbMemo={fbMemo} fbTag={fbTag} tags={tags} />}
+				/>
+				<Route
+					path="/memo/:tagId"
+					element={
+						<MemoPage
+							fbAuth={fbAuth}
+							fbMemo={fbMemo}
+							fbTag={fbTag}
+							tags={tags}
+							userInfo={userInfo}
+						/>
+					}
+				/>
+			</Routes>
+		</MobileBox>
+	);
 };
 
 export default App;
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -3,84 +3,84 @@ import { darken, lighten } from "polished";
 import { fontSizeSet, polishedColor } from "../styles/stylesCss";
 
 interface IMainBtn {
-  primary?: boolean;
-  width?: number;
+	primary?: boolean;
+	width?: number;
 }
 
 const modeSet = css<IMainBtn>`
-  ${({ theme, primary }) => {
-    const backColor = primary
-      ? theme.colors.primary_blue
-      : theme.colors.light_gray;
-    const fontColor = primary && theme.colors.white;
+	${({ theme, primary }) => {
+		const backColor = primary
+			? theme.colors.primary_blue
+			: theme.colors.light_gray;
+		const fontColor = primary && theme.colors.white;
 
-    return css`
-      background: ${backColor};
-      color: ${fontColor};
+		return css`
+			background: ${backColor};
+			color: ${fontColor};
 
-      &:hover {
-        background: ${lighten(0.02, backColor)};
-      }
-      &:active {
-        background: ${darken(0.02, backColor)};
-      }
-    `;
-  }}
+			&:hover {
+				background: ${lighten(0.02, backColor)};
+			}
+			&:active {
+				background: ${darken(0.02, backColor)};
+			}
+		`;
+	}}
 `;
 
 export const MainBtn = styled.button<IMainBtn>`
-  /* 크기 */
-  width: ${({ width }) => (width ? width + "rem" : "5rem")};
-  height: 1.5rem;
-  padding: 0;
-  border-radius: 4px;
-  border: none;
-  cursor: pointer;
+	/* 크기 */
+	width: ${({ width }) => (width ? width + "rem" : "5rem")};
+	height: 1.5rem;
+	padding: 0;
+	border-radius: 4px;
+	border: none;
+	cursor: pointer;
 
-  /* 폰트 */
-  font-size: 0.75rem;
-  font-weight: 600;
+	/* 폰트 */
+	font-size: 0.75rem;
+	font-weight: 600;
 
-  // 그림자 생성
-  box-shadow: ${({ theme }) => theme.boxShadow.main};
+	// 그림자 생성
+	box-shadow: ${({ theme }) => theme.boxShadow.main};
 
-  /* 색상: 모드 */
-  ${modeSet}
+	/* 색상: 모드 */
+	${modeSet}
 `;
 
 interface ICustomBtn {
-  bgColor: string;
-  color?: string;
-  radius?: number;
-  width?: number;
-  padding?: string;
-  whiteSpace?: string;
+	bgColor: string;
+	color?: string;
+	radius?: number;
+	width?: number;
+	padding?: string;
+	whiteSpace?: string;
 
-  fontSize?: string;
-  bold?: boolean;
-  center?: boolean;
-  height?: number;
+	fontSize?: string;
+	bold?: boolean;
+	center?: boolean;
+	height?: number;
 }
 
 export const CustomBtn = styled.button<ICustomBtn>`
-  /* 크기 */
-  width: ${({ width }) => width && width + "rem"};
-  height: ${({ height }) => height && height + "rem"};
-  padding: ${({ padding }) => (padding ? padding : ".5rem")};
-  border-radius: ${({ radius }) => (radius ? radius + "rem" : `.25rem`)};
-  border: none;
+	/* 크기 */
+	width: ${({ width }) => width && width + "rem"};
+	height: ${({ height }) => height && height + "rem"};
+	padding: ${({ padding }) => (padding ? padding : ".5rem")};
+	border-radius: ${({ radius }) => (radius ? radius + "rem" : `.25rem`)};
+	border: none;
 
-  cursor: pointer;
+	cursor: pointer;
 
-  /* 폰트 */
-  ${fontSizeSet}
-  font-weight: ${({ bold }) => bold && "bold"};
+	/* 폰트 */
+	${fontSizeSet}
+	font-weight: ${({ bold }) => bold && "bold"};
 
-  /* 그림자 */
-  box-shadow: ${({ theme }) => theme.boxShadow.main};
-  /* 색상 */
-  color: ${({ color }) => color && color};
-  ${polishedColor}/* overflow 속성 */
+	/* 그림자 */
+	box-shadow: ${({ theme }) => theme.boxShadow.main};
+	/* 색상 */
+	color: ${({ color }) => color && color};
+	${polishedColor}/* overflow 속성 */
   /* display: -webkit-box;
   overflow-y: hidden;
   -webkit-line-clamp: 1;
